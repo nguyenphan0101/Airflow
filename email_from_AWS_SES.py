4a46ebfa76e033c807763b37d4ba544509020954
Nguyen Phan <79673831+nguyenphan0101@users.noreply.github.com>
2024-01-19 09:50:03 +0700

    Update email_from_AWS_SES.py

--- email_from_AWS_SES.py
+++ email_from_AWS_SES.py
@@ -2,8 +2,8 @@
 #Send email from AWS SES
     #AIRFLOW__EMAIL__EMAIL_BACKEND: airflow.providers.amazon.aws.utils.emailer.send_email
     #AIRFLOW__EMAIL__EMAIL_CONN_ID: aws_default
-    #AIRFLOW__EMAIL__MAIL_FROM: From email nguyenphan01012002@gmail.com  #success@simulator.amazones.com
-    #AIRFLOW__EMAIL__FROM_EMAIL: nguyenphan01012002@gmail.com #success@simulator.amazones.com
+    #AIRFLOW__EMAIL__MAIL_FROM: From email nguyenphan01012002@gmail.com 
+    #AIRFLOW__EMAIL__FROM_EMAIL: nguyenphan01012002@gmail.com
     #AIRFLOW__EMAIL__SMTP_HOST: email-smtp.ap-southeast-1.amazonaws.com
 
 from airflow import DAG
@@ -19,7 +19,7 @@ with DAG(dag_id="Email_From_AWS_SES",start_date=datetime(2024,1,1),schedule_inte
         task_id="email_function_2",
         to="nguyenphan01012002@gmail.com",
         subject="Alert Mail",
-        html_content="""Error.Check the DAGs now!"""
+        html_content="Error. Check the DAGs now!"
     )
 
 email_function
